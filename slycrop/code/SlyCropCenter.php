b64066850e61a0c8958232d6607e72dfe3664d79
Stig Lindqvist <stig@stojg.net>
2012-04-30 10:33:12 +1200

    MINOR Update the inline documentation for the SlyCropCenter

--- slycrop/code/SlyCropCenter.php
+++ slycrop/code/SlyCropCenter.php
@@ -1,13 +1,37 @@
 <?php
 /**
  * SlyCropCenter
+ * 
+ * The most basic of cropping techniques:
+ *
+ * 1. Find the exact center of the image
+ * 2. Trim any edges that is bigger than the targetWidth and targetHeight
  *
  */
 class SlyCropCenter extends SlyCrop {
 
 	/**
-	 * Plain old boring cropping from the center of the image
-     *
+	 *
+	 * @param string $imagePath
+	 * @param int $targetWidth
+	 * @param int $targetHeight
+	 * @return boolean|\Imagick
+	 */
+	public function resizeAndCrop($targetWidth, $targetHeight) {
+		// First get the size that we can use to safely trim down the image to without cropping any sides
+		$crop = $this->getSafeResizeOffset($this->originalImage, $targetWidth, $targetHeight);
+		// Resize image
+		$this->originalImage->resizeImage($crop['width'], $crop['height'], Imagick::FILTER_CATROM, 0.5);
+		// Get the offset from the center of the image
+		$offset = $this->getCenterOffset($this->originalImage, $targetWidth, $targetHeight);
+		// Crop the image
+		$this->originalImage->cropImage($targetWidth, $targetHeight, $offset['x'], $offset['y']);
+		return $this->originalImage;
+	}
+
+	/**
+	 * Get the cropping offset for the image based on the center of the image
+	 *
 	 * @param Imagick $image
 	 * @param int $targetWidth
 	 * @param int $targetHeight
@@ -22,21 +46,4 @@ class SlyCropCenter extends SlyCrop {
 		return array('x' => $goalX, 'y' => $goalY);
 	}
 
-	
-	/**
-	 *
-	 * @param string $imagePath
-	 * @param int $targetWidth
-	 * @param int $targetHeight
-	 * @return boolean|\Imagick
-	 */
-	public function resizeAndCrop($targetWidth, $targetHeight) {
-		// First get the size that we can use to safely trim down the image to
-		// without cropping any sides
-		$crop = $this->getSafeResizeOffset($this->originalImage, $targetWidth, $targetHeight);
-		$this->originalImage->resizeImage($crop['width'], $crop['height'], Imagick::FILTER_CATROM, 0.5);
-		$offset = $this->getCenterOffset($this->originalImage, $targetWidth, $targetHeight);
-		$this->originalImage->cropImage($targetWidth, $targetHeight, $offset['x'], $offset['y']);
-		return $this->originalImage;
-	}
 }
