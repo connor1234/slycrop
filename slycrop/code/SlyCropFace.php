7136322c85fa63f17726ff19a58b2537eeb82f47
Stig Lindqvist <stig@stojg.net>
2012-04-29 12:48:36 +1200

    MINOR Added to for the next crop

---
+++ slycrop/code/SlyCropFace.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * SlyCropFace
+ *
+ * @todo implement, maybe get some helps from https://github.com/mauricesvay/php-facedetection/blob/master/FaceDetector.php
+ */
+class SlyCropFace extends SlyCrop {
+
+}
